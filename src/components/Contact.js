adea87aba7de7869c7e7214bcc9c4f382128ab34
Ronald Winkler <rwinkler.dev@gmail.com>
2023-01-29 12:21:12 -0600

    Updated titles, styles, & resume

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -36,7 +36,7 @@ class Contact extends React.Component {
                             <div id="copy-email-alert" className="hide-alert">Email copied to clipboard!</div>
                             <div><i className="fa-solid fa-file-pdf" />: <a href="/docs/Winkler_Ronald_Resume.pdf" className="info-link" target="_blank" rel="noreferrer" title="Resume">Resume</a></div>
                             <div><i className="fa-regular fa-envelope" />: <span onClick={this.copyEmail} id="email-link"  className="info-link" title="Copy email to clipboard">Rwinkler.dev@gmail.com</span></div>
-                            <div><i className="fa-brands fa-linkedin" />: <a href="https://linkedin.com/in/ronaldwinkler" className="info-link" target="_blank" rel="noreferrer" title="Linkedin">Https://linkedin.com/in/ronaldwinkler</a></div>
+                            <div><i className="fa-brands fa-linkedin" />: <a href="https://linkedin.com/in/ronaldwinkler/" className="info-link" target="_blank" rel="noreferrer" title="Linkedin">Https://linkedin.com/in/ronaldwinkler</a></div>
                             <div><i className="fa-brands fa-square-twitter" />: <a href="https://twitter.com/rwinklertx" className="info-link" target="_blank" rel="noreferrer" title="Twitter">Https://twitter.com/rwinklertx</a></div>
                             
                         </div>
